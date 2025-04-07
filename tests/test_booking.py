03b6703f0c1556c637ffa8587114408b3770fcbb
dsmnv <dmitry.d.sem@gmail.com>
2025-04-07 21:00:14 +0200

    delete_tests added

--- tests/test_booking.py
+++ tests/test_booking.py
@@ -83,6 +83,31 @@ def test_update_token_no_auth(base_url):
         assert response.status_code == 403
 
 
+#def test_partial_update_booking(base_url, create_booking):
+#    pass()
+
+def test_delete_booking(base_url, create_booking, auth_token):
+    booking = create_booking
+    booking_id = booking['id']
+
+    response = requests.delete(f'{base_url}/booking/{booking_id}', headers={
+        'Cookie': f'token={auth_token}'
+    })
+    with allure.step('Пользователь удален успешно'):
+        assert response.status_code == 201
+
+    updated_response = requests.get(f'{base_url}/booking/{booking_id}')
+    with allure.step('Удаленного пользователя нет в БД'):
+        assert updated_response.status_code == 404
+
+
+def test_delete_booking_no_auth(base_url):
+    response = requests.delete(f'{base_url}/booking/1')
+
+    with allure.step('Удаление без авторизации невозможно'):
+        assert response.status_code == 403
+
+
 
 
 
