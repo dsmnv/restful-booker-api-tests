e217f0e1261a41953becc4e32a25b47736b941b4
dsmnv <dmitry.d.sem@gmail.com>
2025-04-16 20:04:39 +0200

    Added automatic date generation

--- utils/data_generator.py
+++ utils/data_generator.py
@@ -1,16 +1,20 @@
 from faker import Faker
+from datetime import date, timedelta
 import random
 fake = Faker()
 
 
 def get_booking_payload():
+    checkin = date.today()
+    checkout = checkin + timedelta(days=random.randint(1, 14))
+
     firstname = fake.first_name()
     lastname = fake.last_name()
     totalprice = fake.random_int(10, 3000)
     depositpaid = fake.boolean()
     bookingdates = {
-        'checkin': '2018-01-01',
-        'checkout': "2019-01-01"
+        'checkin': checkin.isoformat(),
+        'checkout': checkout.isoformat()
     }
     additionalneeds = random.choice(['Breakfast', 'SeaView', 'All Inclusive'])
 
